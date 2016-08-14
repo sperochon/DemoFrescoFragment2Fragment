87f39acca6893fe3906bad8cdad35929acfdeec3
Sébastien Pérochon <sebastien.perochon@mobiquite.fr>
2016-08-14 22:40:43 +0200

    Bug: Use fragment_end instead of same item (cardview) and change size of img

--- app/src/main/java/com/example/android/EndFragment.java
+++ app/src/main/java/com/example/android/EndFragment.java
@@ -25,7 +25,7 @@ public class EndFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.cardview, container, false);
+    View view = inflater.inflate(R.layout.fragment_end, container, false);
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
       SimpleDraweeView draweeView = (SimpleDraweeView) view.findViewById(R.id.img);
